b9bc7e17da1c29c48f60b8eba3ba224e484eef36
devanshjain <devanshjain@microsoft.com>
2024-03-06 21:22:08 +0000

    Update logicapp.tf file

--- deployer/terraform/sap_bgprint/logicapp.tf
+++ deployer/terraform/sap_bgprint/logicapp.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group_template_deployment" "name" {
         }
     })
     template_content = <<TEMPLATE
-        {
+    {
         "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
         "contentVersion": "1.0.0.0",
         "variables": {},
@@ -35,7 +35,7 @@ resource "azurerm_resource_group_template_deployment" "name" {
             "clientId": {
                 "type": "string"
             }
-        }
+        },
         "resources": [
             {
                 "type": "Microsoft.Web/customApis",
