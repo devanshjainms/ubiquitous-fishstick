d22633ee8fc7f344ca30aa99bf7f54786087da92
devanshjain <devanshjain@microsoft.com>
2024-03-07 01:09:46 +0000

    Update logic app custom connector name

--- deployer/terraform/sap_bgprint/logicapp.tf
+++ deployer/terraform/sap_bgprint/logicapp.tf
@@ -1,6 +1,6 @@
 # Logic app custom connector
 resource "azurerm_resource_group_template_deployment" "name" {
-    name                = format("%s-%s", lower(random_string.random.override_special), lower(var.location))
+    name                = format("%s-%s", lower(random_string.random.result), lower(var.location))
     resource_group_name = azurerm_resource_group.rg.name
     deployment_mode     = "Incremental"
     parameters_content  = jsonencode({
