bf36a58c4f87308367a37f039b9b3275e4b50b3b
devanshjain <devanshjain@microsoft.com>
2024-03-12 18:06:50 +0000

    Add custom connector output to terraform

--- deployer/terraform/outputs.tf
+++ deployer/terraform/outputs.tf
@@ -14,3 +14,8 @@ output "key_vault_id" {
     description = "The id of the key vault"
     value       = module.sap_bgprint.key_vault_id
 }
+
+output "custom_connector" {
+    description = "The id of the custom connector"
+    value       = module.sap_bgprint.custom_connector
+}
